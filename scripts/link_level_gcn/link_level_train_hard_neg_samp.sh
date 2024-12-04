e5e737d37d776b7b52f9f9aba0a5617a8a913088
derrickzhuyz <derrickzhuyz@outlook.com>
2024-12-04 15:53:53 +0000

    [update] organize bash scripts

---
+++ scripts/link_level_gcn/link_level_train_hard_neg_samp.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Set the environment variables if needed
+# export VARIABLE_NAME=value
+
+# Set the visible GPU(s)
+export CUDA_VISIBLE_DEVICES='7'
+
+# Train the link-level model
+echo "[INFO] Start training of the link-level model..."
+python -m gnn.model.link_level_train \
+    --model_type "gcn" \
+    --dataset_type "bird" \
+    --epochs 200 \
+    --batch_size 1 \
+    --threshold 0.5 \
+    --val_ratio 0.1 \
+    --val_dataset_type "bird" \
+    --lr 1e-3 \
+    --embed_method "sentence_transformer" \
+    --in_channels 384 \
+    --prediction_method "dot_product" \
+    --negative_sampling \
+    --negative_sampling_ratio 1.0 \
+    --negative_sampling_method "hard" \
+    --metric "f1"
+
+
+python -m gnn.model.link_level_train \
+    --model_type "gcn" \
+    --dataset_type "bird" \
+    --epochs 200 \
+    --batch_size 1 \
+    --threshold 0.5 \
+    --val_ratio 0.1 \
+    --val_dataset_type "bird" \
+    --lr 1e-3 \
+    --embed_method "api_small" \
+    --in_channels 1536 \
+    --prediction_method "dot_product" \
+    --negative_sampling \
+    --negative_sampling_ratio 1.0 \
+    --negative_sampling_method "hard" \
+    --metric "f1"
+
+# -----------------------------------------
+
+python -m gnn.model.link_level_train \
+    --model_type "gcn" \
+    --dataset_type "bird" \
+    --epochs 200 \
+    --batch_size 1 \
+    --threshold 0.5 \
+    --val_ratio 0.1 \
+    --val_dataset_type "bird" \
+    --lr 1e-3 \
+    --embed_method "sentence_transformer" \
+    --in_channels 384 \
+    --prediction_method "dot_product" \
+    --negative_sampling \
+    --negative_sampling_ratio 1.0 \
+    --negative_sampling_method "hard" \
+    --metric "auc"
+
+
+python -m gnn.model.link_level_train \
+    --model_type "gcn" \
+    --dataset_type "bird" \
+    --epochs 200 \
+    --batch_size 1 \
+    --threshold 0.5 \
+    --val_ratio 0.1 \
+    --val_dataset_type "bird" \
+    --lr 1e-3 \
+    --embed_method "api_small" \
+    --in_channels 1536 \
+    --prediction_method "dot_product" \
+    --negative_sampling \
+    --negative_sampling_ratio 1.0 \
+    --negative_sampling_method "hard" \
+    --metric "auc"
